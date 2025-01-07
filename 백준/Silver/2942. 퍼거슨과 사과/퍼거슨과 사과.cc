d83013b8b4cc94e3e76ddff244575c1cc1c7a0e4
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-01-08 02:23:58 +0900

    [Silver II] Title: 퍼거슨과 사과, Time: 236 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2942. 퍼거슨과 사과/퍼거슨과 사과.cc
+++ 백준/Silver/2942. 퍼거슨과 사과/퍼거슨과 사과.cc
@@ -8,37 +8,21 @@ typedef long long ll;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1 ,1};
-int R, G;
 
-int divisor(int a, int b) {
-    int cnt = 1;
-    bool visited = false;
-
-    while(true) {
-        visited = false;
-        for(int i = 2; i <= min(a, b); i++) {
-            if(a % i == 0 && b % i == 0) {
-                cnt *= i;
-                a /= i;
-                b /= i;
-                visited = true;
-                break;
-            }
-        }
-        if(!visited) {
-            return cnt;
-        }
-    }
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    return gcd(b, a%b);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-    int tmp;
+    int R, G, tmp;
 
     cin >> R >> G;
 
-    tmp = divisor(R, G);
+    tmp = gcd(R, G);
+
     for(int i = 1; i <= tmp; i++) {
         if(tmp % i == 0) {
             cout << i << " " << R / i << " " << G / i << endl;
