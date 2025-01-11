e821a4b9b8effe2279a09fbb54c15cac3b54b484
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-01-12 02:49:39 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24228. 젓가락/젓가락.cc
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -1,4 +1,4 @@
-// baekjoon 28706
+// baekjoon 24228
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> pii;
@@ -8,22 +8,13 @@ typedef long long ll;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1 ,1};
+ll N, R;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
-    ll N, R;
-
     cin >> N >> R;
 
-//    if(N == 1) {
-//        if(R == 1) {
-//            cout << 2;
-//            return 0;
-//        }
-//        cout << R;
-//        return 0;
-//    }
-    cout << N + (R * 2) - 1;
+    cout << N + R * 2 - 1;
 }
