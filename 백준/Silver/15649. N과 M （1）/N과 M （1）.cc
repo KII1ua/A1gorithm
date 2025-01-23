5b76eeac1883160093e640c7a3a4ca3ed1ca5813
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-01-24 00:20:07 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,40 @@
+// baekjoon 15649
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define INF int(1e9)
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1 ,1};
+int N, M;
+bool visited[9];
+int arr[9];
+
+void bt(int x) {
+    if(x == M) {
+        for(int j = 0; j < M; j++) {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for(int i = 1; i <= N; i++) {
+        if(visited[i] == true) continue;
+        visited[i] = true;
+        arr[x] = i;
+        bt(x+1);
+        visited[i] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    cin >> N >> M;
+
+    bt(0);
+}
