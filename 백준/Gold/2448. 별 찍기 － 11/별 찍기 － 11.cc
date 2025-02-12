266f2744b9e29213e6bf3304084ebc98676cd0f4
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-02-12 23:20:29 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 368 ms, Memory: 99676 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.cc
@@ -0,0 +1,52 @@
+// baekjoon 17835
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define INF int(1e9)
+
+int dx[9] = {-1, 1, 0, 0, -1, -1, 1, 1, 0};
+int dy[9] = {0, 0, -1, 1, -1, 1, -1, 1, 0};
+char graph[10000][10000];
+int N;
+
+void draw(int x, int y, int n) {
+    if(n == 3) {
+        graph[x][y] = '*';
+        graph[x+1][y-1] = '*';
+        graph[x+1][y+1] = '*';
+        graph[x+2][y-2] = '*';
+        graph[x+2][y-1] = '*';
+        graph[x+2][y] = '*';
+        graph[x+2][y+1] = '*';
+        graph[x+2][y+2] = '*';
+    }
+    else {
+        draw(x, y, n / 2);
+        draw(x + n / 2, y - n / 2, n / 2);
+        draw(x + n / 2, y + n / 2, n / 2);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < 2 * N; j++) {
+            graph[i][j] = ' ';
+        }
+    }
+
+    draw(0, N-1, N);
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < 2 * N; j++) {
+            cout << graph[i][j];
+        }
+        cout << endl;
+    }
+}
