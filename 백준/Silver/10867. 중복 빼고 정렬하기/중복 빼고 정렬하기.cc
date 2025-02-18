0af8e1860c3275e256e5c4d47928ccc9da3fb445
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-02-19 00:30:49 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 12 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.cc
@@ -0,0 +1,29 @@
+// baekjoon 17835
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define INF int(1e9)
+
+int dx[9] = {-1, 1, 0, 0, -1, -1, 1, 1, 0};
+int dy[9] = {0, 0, -1, 1, -1, 1, -1, 1, 0};
+int N;
+set<int> s;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        s.insert(a);
+    }
+
+    for(auto it: s) {
+        cout << it << " ";
+    }
+}
