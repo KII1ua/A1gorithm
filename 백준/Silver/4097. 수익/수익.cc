7c265838fd6b13e698ea58a66b0f58b1ef413686
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-02-25 01:59:10 +0900

    [Silver II] Title: 수익, Time: 84 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/4097. 수익/수익.cc
+++ 백준/Silver/4097. 수익/수익.cc
@@ -8,33 +8,39 @@ typedef long long ll;
 
 int dx[9] = {-1, 1, 0, 0, -1, -1, 1, 1, 0};
 int dy[9] = {0, 0, -1, 1, -1, 1, -1, 1, 0};
+int dp[250001];
 int N;
-int arr[250001];
+
+int solution() {
+    int result = -10001;
+
+    for(int i = 1; i < N; i++) {
+        dp[i] = max(dp[i] + dp[i-1], dp[i]);
+    }
+
+    for(int i = 0; i < N; i++) {
+        result = max(result, dp[i]);
+    }
+
+    return result;
+}
+
+void input() {
+    while(true) {
+        cin >> N;
+        if(!N) return;
+
+        for(int i = 0; i < N; i++) {
+            cin >> dp[i];
+        }
+
+        cout << solution() << endl;
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
-    while(true) {
-        cin >> N;
-        if(!N) return 0;
-
-        int result = -10001;
-
-        for(int i = 1; i <= N; i++) {
-            cin >> arr[i];
-        }
-
-        for(int i = 2; i <= N; i++) {
-            arr[i] = max(arr[i] + arr[i-1], arr[i]);
-        }
-
-        for(int i = 1; i <= N; i++) {
-            result = max(result, arr[i]);
-        }
-
-        cout << result << endl;
-
-        memset(arr, 0, sizeof(arr));
-    }
+    input();
 }
