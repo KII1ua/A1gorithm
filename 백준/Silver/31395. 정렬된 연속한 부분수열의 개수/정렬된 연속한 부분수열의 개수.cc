4ed27d0de7f26a7becf21ff38ebeb4ede803629d
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-03-22 22:04:46 +0900

    [Silver IV] Title: 정렬된 연속한 부분수열의 개수, Time: 20 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Silver/31395. 정렬된 연속한 부분수열의 개수/정렬된 연속한 부분수열의 개수.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+int N;
+ll arr[200002];
+ll result;
+
+void solve() {
+    int cnt = 0;
+
+    for(int i = 1; i <= N; i++) {
+        if(arr[i] <= arr[i+1]) {
+            cnt++;
+            result += cnt;
+        }
+        else {
+            cnt++;
+            result += cnt;
+            cnt = 0;
+        }
+    }
+
+    cout << result;
+}
+
+void input() {
+    cin >> N;
+
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    arr[N+1] = 0;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
