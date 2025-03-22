a4d0a21c89b2cfd8329a82ff9f5c3bf5ad6ee0e6
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-03-23 00:23:58 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 8 ms, Memory: 4060 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.cc
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.cc
@@ -1,41 +1,38 @@
-// baekjoon 17835
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
 #define endl "\n"
-#define INF int(1e9)
 
-int dx[9] = {-1, 1, 0, 0, -1, -1, 1, 1, 0};
-int dy[9] = {0, 0, -1, 1, -1, 1, -1, 1, 0};
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 int A, K;
 bool visited[1000001];
 
-void bfs(int a) {
+void bfs() {
     queue<pii> q;
-
-    q.push(make_pair(a, 0));
-    visited[a] = true;
+    q.push(make_pair(A, 0));
+    visited[A] = true;
 
     while(!q.empty()) {
-        int res = q.front().first;
+        int x = q.front().first;
         int cnt = q.front().second;
         q.pop();
 
-        if(res == K) {
+        if(x == K) {
             cout << cnt;
             return;
         }
 
-        int arr[] = {res + 1, res * 2};
+        int xx[] = {x + 1, x * 2};
 
         for(int i = 0; i < 2; i++) {
-            int stand = arr[i];
+            int nx = xx[i];
 
-            if(stand >= 1 && stand <= 1000000) {
-                if(!visited[stand]) {
-                    q.push(make_pair(stand, cnt+1));
-                    visited[stand] = true;
+            if(nx >= 1 && nx <= 1000000) {
+                if(!visited[nx]) {
+                    q.push(make_pair(nx, cnt + 1));
+                    visited[nx] = true;
                 }
             }
         }
@@ -48,7 +45,7 @@ void input() {
 
 void solution() {
     input();
-    bfs(A);
+    bfs();
 }
 
 int main() {
