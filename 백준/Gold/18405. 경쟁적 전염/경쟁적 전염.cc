0b3db9baa0b64dd3c92161ae33ab93c495e2ec87
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-07-05 00:23:23 +0900

    [Gold V] Title: 경쟁적 전염, Time: 28 ms, Memory: 2456 KB -BaekjoonHub

---
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.cc
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+};
+
+int dx[] = {-1, 0, 0, 1, 1, -1, -1, 1};
+int dy[] = {0, -1, 1, 0, -1, 1, -1, 1};
+//int dx[] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
+//int dy[] = {0,0, -1, -1, -1, 0, 1, 1, 1};
+//int dx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
+//int dy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
+int N, K, S, X, Y;
+int graph[201][201];
+vector<vector<pii>> v(1001);
+
+void solve() {
+    for(int i = 0; i < S; i++) {
+        for(int j = 1; j <= K; j++) {
+            vector<pii> tmp;
+            for(auto it : v[j]) {
+                int x = it.first;
+                int y = it.second;
+
+
+                for(int k = 0; k < 4; k++) {
+                    int nx = x + dx[k];
+                    int ny = y + dy[k];
+
+                    if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+                    if(graph[nx][ny] != 0) continue;
+
+                    graph[nx][ny] = j;
+                    tmp.push_back({nx, ny});
+                }
+            }
+            v[j] = tmp;
+        }
+    }
+
+    cout << graph[X-1][Y-1];
+}
+
+void input() {
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            cin >> graph[i][j];
+
+            if(graph[i][j] != 0) {
+                v[graph[i][j]].push_back({i, j});
+            }
+        }
+    }
+
+    cin >> S >> X >> Y;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+
+//    int row = 3;
+//    int column = 4;
+//
+//    int **arr = (int**)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    cout << arr[0][0];
+
+
+//    int **arr = (int **)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            arr[i][j] = j;
+//        }
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    cout << sizeof(int);
+//    cout << sizeof(int*);
+
+
+//    int row = 3;
+//
+//    int *arr = (int*)malloc(10000 * sizeof(int));
+//
+//    for(int i = 0; i < 10000; i++) {
+//        arr[i] = i;
+//    }
+//
+//    for(int i = 0; i < 100; i++) {
+//        cout << arr[i] << endl;
+//    }
+//
+//    free(arr);
+
+
+//    int arr[] = {1, 2, 3, 4, 5};
+//    int *ptr = arr;
+//    int **tmp = ptr;
+
+//    struct pointwrapper pwrap;
+//    struct jsu st = {"데이터", 4, 5, 4, 3};
+//
+//    pwrap.ptr = &st;
+//
+//    cout << pwrap.ptr << endl;
+//
+//    cout << pwrap.ptr -> os;
+
+//    struct jsu st[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91},
+//                        {"데이터3", 86, 75}};
+//
+//    struct jsu* p;
+//
+//    p = &st[0];
+//
+//    (p + 1) -> hab = (p + 1) -> os + (p + 2) -> db;
+//
+//    cout << p;
+//    cout << *p.os;
+}
