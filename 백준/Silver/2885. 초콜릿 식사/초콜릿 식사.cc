129d8c3f063c333fe6cd44247e31803a3d89fa2b
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-07-11 01:27:03 +0800

    [Silver II] Title: 초콜릿 식사, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.cc
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+int dx[] = {0, 1, -1, 0,1, -1, -1, 1};
+int dy[] = {1, 0, 0, -1, -1, 1, -1, 1};
+int K;
+
+void solve() {
+    int ans = 0;
+    int cut = 0;
+
+    for(int i = 0; i < K; i++) {
+        int tmp = pow(2, i);
+
+        if(tmp >= K) {
+            ans = pow(2, i);
+            break;
+        }
+    }
+
+    if(ans == K) {
+        cout << ans << " " << 0;
+        return;
+    }
+    else {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        pq.push(ans);
+        int res = 0;
+        while(true) {
+            if(res == K) {
+                break;
+            }
+
+            if(res + pq.top() > K) {           // 분할해야함
+                int each = pq.top();
+                pq.pop();
+                each /= 2;
+                for(int i = 0; i < 2; i++) {
+                    pq.push(each);
+                }
+                cut++;
+            }
+            else {
+                if(res + pq.top() <= K) {
+                    int each = pq.top();
+                    pq.pop();
+                    res += each;
+                }
+            }
+        }
+    }
+
+    cout << ans << " " << cut;
+}
+
+void input() {
+    cin >> K;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+
+//    int row = 3;
+//    int column = 4;
+//
+//    int **arr = (int**)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    cout << arr[0][0];
+
+
+//    int **arr = (int **)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            arr[i][j] = j;
+//        }
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    cout << sizeof(int);
+//    cout << sizeof(int*);
+
+
+//    int row = 3;
+//
+//    int *arr = (int*)malloc(10000 * sizeof(int));
+//
+//    for(int i = 0; i < 10000; i++) {
+//        arr[i] = i;
+//    }
+//
+//    for(int i = 0; i < 100; i++) {
+//        cout << arr[i] << endl;
+//    }
+//
+//    free(arr);
+
+
+//    int arr[] = {1, 2, 3, 4, 5};
+//    int *ptr = arr;
+//    int **tmp = ptr;
+
+//    struct pointwrapper pwrap;
+//    struct jsu st = {"데이터", 4, 5, 4, 3};
+//
+//    pwrap.ptr = &st;
+//
+//    cout << pwrap.ptr << endl;
+//
+//    cout << pwrap.ptr -> os;
+
+//    struct jsu st[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91},
+//                        {"데이터3", 86, 75}};
+//
+//    struct jsu* p;
+//
+//    p = &st[0];
+//
+//    (p + 1) -> hab = (p + 1) -> os + (p + 2) -> db;
+//
+//    cout << p;
+//    cout << *p.os;
+}
