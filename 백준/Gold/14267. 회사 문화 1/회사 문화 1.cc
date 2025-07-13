fb576b1513a547499681c311c239e63613072f7a
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-07-13 18:08:37 +0900

    [Gold IV] Title: 회사 문화 1, Time: 36 ms, Memory: 10920 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.cc
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+int dx[] = {0, 1, -1, 0,1, -1, -1, 1};
+int dy[] = {1, 0, 0, -1, -1, 1, -1, 1};
+int N, M;
+int dp[100001];
+vector<vector<int>> v(100001);
+
+void dfs(int x) {
+
+    for(auto it : v[x]) {
+        dp[it] += dp[x];
+        dfs(it);
+    }
+}
+
+void solve() {
+    dfs(1);
+
+    for(int i = 1; i <= N; i++) {
+        cout << dp[i] << " ";
+    }
+}
+
+void input() {
+    cin >> N >> M;
+
+    for(int i = 1; i <= N; i++) {
+        int a;
+        cin >> a;
+        if(a != -1) {
+            v[a].push_back(i);
+        }
+    }
+
+    for(int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        dp[a] += b;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+
+//    int row = 3;
+//    int column = 4;
+//
+//    int **arr = (int**)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    cout << arr[0][0];
+
+
+//    int **arr = (int **)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            arr[i][j] = j;
+//        }
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    cout << sizeof(int);
+//    cout << sizeof(int*);
+
+
+//    int row = 3;
+//
+//    int *arr = (int*)malloc(10000 * sizeof(int));
+//
+//    for(int i = 0; i < 10000; i++) {
+//        arr[i] = i;
+//    }
+//
+//    for(int i = 0; i < 100; i++) {
+//        cout << arr[i] << endl;
+//    }
+//
+//    free(arr);
+
+
+//    int arr[] = {1, 2, 3, 4, 5};
+//    int *ptr = arr;
+//    int **tmp = ptr;
+
+//    struct pointwrapper pwrap;
+//    struct jsu st = {"데이터", 4, 5, 4, 3};
+//
+//    pwrap.ptr = &st;
+//
+//    cout << pwrap.ptr << endl;
+//
+//    cout << pwrap.ptr -> os;
+
+//    struct jsu st[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91},
+//                        {"데이터3", 86, 75}};
+//
+//    struct jsu* p;
+//
+//    p = &st[0];
+//
+//    (p + 1) -> hab = (p + 1) -> os + (p + 2) -> db;
+//
+//    cout << p;
+//    cout << *p.os;
+}
