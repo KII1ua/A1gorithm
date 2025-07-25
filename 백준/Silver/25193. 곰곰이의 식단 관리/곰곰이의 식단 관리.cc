76b24e2e19115ddf84c897f011eeb3e869010074
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-07-26 01:05:21 +0900

    [Silver V] Title: 곰곰이의 식단 관리, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Silver/25193. 곰곰이의 식단 관리/곰곰이의 식단 관리.cc
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+int dx[] = {-1, 0, 1, 0, 1, -1, -1, 1};
+int dy[] = {0, 1, 0, -1, -1, 1, -1, 1};
+string s;
+int N, chicken, food;
+
+void solve() {
+    if(food == 0) {
+        cout << chicken;
+        return;
+    }
+    if(chicken % (food+1) == 0) {
+        cout << chicken / (food+1);
+    }
+    else {
+        cout << (chicken / (food+1)) + 1;
+    }
+}
+
+void input() {
+    cin >> N >> s;
+
+    for(int i = 0; i < N; i++) {
+        if(s[i] == 'C') chicken++;
+        else food++;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+
+//    int row = 3;
+//    int column = 4;
+//
+//    int **arr = (int**)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    cout << arr[0][0];
+
+
+//    int **arr = (int **)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            arr[i][j] = j;
+//        }
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    cout << sizeof(int);
+//    cout << sizeof(int*);
+
+
+//    int row = 3;
+//
+//    int *arr = (int*)malloc(10000 * sizeof(int));
+//
+//    for(int i = 0; i < 10000; i++) {
+//        arr[i] = i;
+//    }
+//
+//    for(int i = 0; i < 100; i++) {
+//        cout << arr[i] << endl;
+//    }
+//
+//    free(arr);
+
+
+//    int arr[] = {1, 2, 3, 4, 5};
+//    int *ptr = arr;
+//    int **tmp = ptr;
+
+//    struct pointwrapper pwrap;
+//    struct jsu st = {"데이터", 4, 5, 4, 3};
+//
+//    pwrap.ptr = &st;
+//
+//    cout << pwrap.ptr << endl;
+//
+//    cout << pwrap.ptr -> os;
+
+//    struct jsu st[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91},
+//                        {"데이터3", 86, 75}};
+//
+//    struct jsu* p;
+//
+//    p = &st[0];
+//
+//    (p + 1) -> hab = (p + 1) -> os + (p + 2) -> db;
+//
+//    cout << p;
+//    cout << *p.os;
+}
