8e4e723f05a329e666774ed18212b6557d550717
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-07-28 22:33:50 +0900

    [Silver III] Title: 두 개의 배열, Time: 260 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/17124. 두 개의 배열/두 개의 배열.cc
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+};
+
+int dx[] = {-1, 0, 1, 0, 1, -1, -1, 1};
+int dy[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int T, N, M;
+vector<int> arr1;
+vector<int> arr2;
+
+void solve() {
+    sort(arr2.begin(), arr2.end());
+
+    ll result = 0;
+
+    for(int i = 0; i < N; i++) {
+        int start = 0;
+        int end = M-1;
+        int tmp = MAX;
+        int res = 0;
+        int stand = arr1[i];
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            int val = arr2[mid];
+
+            int vals = abs(stand - val);
+
+            if(vals < tmp) {
+                tmp = vals;
+                res = val;
+            }
+            else if(vals == tmp) {
+                if(res > val) res = val;
+            }
+
+            if(vals == 0) break;
+
+            if(stand > val) {
+                start = mid+1;
+            }
+            else {
+                end = mid-1;
+            }
+
+        }
+        result += res;
+    }
+
+    cout << result << endl;
+}
+
+void input() {
+    cin >> T;
+
+    while(T--) {
+        cin >> N >> M;
+
+        for(int i = 0; i < N; i++) {
+            int a;
+            cin >> a;
+            arr1.push_back(a);
+        }
+
+        for(int i = 0; i < M; i++) {
+            int a;
+            cin >> a;
+            arr2.push_back(a);
+        }
+
+        solve();
+
+        arr1.clear();
+        arr2.clear();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+
+//    int row = 3;
+//    int column = 4;
+//
+//    int **arr = (int**)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    cout << arr[0][0];
+
+
+//    int **arr = (int **)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            arr[i][j] = j;
+//        }
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    cout << sizeof(int);
+//    cout << sizeof(int*);
+
+
+//    int row = 3;
+//
+//    int *arr = (int*)malloc(10000 * sizeof(int));
+//
+//    for(int i = 0; i < 10000; i++) {
+//        arr[i] = i;
+//    }
+//
+//    for(int i = 0; i < 100; i++) {
+//        cout << arr[i] << endl;
+//    }
+//
+//    free(arr);
+
+
+//    int arr[] = {1, 2, 3, 4, 5};
+//    int *ptr = arr;
+//    int **tmp = ptr;
+
+//    struct pointwrapper pwrap;
+//    struct jsu st = {"데이터", 4, 5, 4, 3};
+//
+//    pwrap.ptr = &st;
+//
+//    cout << pwrap.ptr << endl;
+//
+//    cout << pwrap.ptr -> os;
+
+//    struct jsu st[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91},
+//                        {"데이터3", 86, 75}};
+//
+//    struct jsu* p;
+//
+//    p = &st[0];
+//
+//    (p + 1) -> hab = (p + 1) -> os + (p + 2) -> db;
+//
+//    cout << p;
+//    cout << *p.os;
+}
