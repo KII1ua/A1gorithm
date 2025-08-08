28f3d348902a28557bf486ec56e775051dfe8499
j11gen <86961575+KII1ua@users.noreply.github.com>
2025-08-08 18:12:07 +0900

    [Silver V] Title: 또 수열 문제야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    string s;
+    vector<int> v;
+};
+
+int dx[] = {-1, 0, 1, 0, 1, -1, -1, 1};
+int dy[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int N;
+
+void solve() {
+    int cnt = 0;
+
+    for(int i = 1; i < MAX; i++) {
+        if(i % 2 == 1) {
+            cout << i << " ";
+            cnt++;
+        }
+
+        if(cnt == N) break;
+    }
+}
+
+void input() {
+    cin >> N;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
