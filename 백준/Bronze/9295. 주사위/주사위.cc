a0ed058f2d579049c75586d310e4f9b38bb9ef2a
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-10-06 04:06:20 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+    int r;
+};
+
+int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
+int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+int T;
+int cnt = 1;
+
+void solve() {
+}
+
+void input() {
+    cin >> T;
+
+    while(T--) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case " << cnt++ << ": " << a + b << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
