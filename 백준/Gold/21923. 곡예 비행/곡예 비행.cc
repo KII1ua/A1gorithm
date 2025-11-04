8c71d9427124b314d7303dcf489a9a2be8d5c371
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-11-04 21:15:41 +0900

    [Gold IV] Title: 곡예 비행, Time: 112 ms, Memory: 9864 KB -BaekjoonHub

--- 백준/Gold/21923. 곡예 비행/곡예 비행.cc
+++ 백준/Gold/21923. 곡예 비행/곡예 비행.cc
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
@@ -23,63 +23,42 @@ struct halloween {
 int dx[] = {0, -1, 1, 0};
 int dy[] = {1, 0, 0, 1};
 int N, M;
-int graph[1001][1001];
-int dp[1001][1001][2];
-
-int func(int x, int y, int status) {
-    int &ret = dp[x][y][status];
-    if(ret != -1) return ret;
-
-    int score = graph[x][y];
-
-    if(x == N-1 && y == M-1 && !status) return score;
-    ret = -MAX;
-
-    if(status) {        // 올라가기
-        if(x - 1 >= 0) {
-            ret = max(ret, func(x-1, y, 1) + score);
-        }
-
-        if(y + 1 <= M - 1) {
-            ret = max(ret, func(x, y+1, 1) + score);
-        }
-
-        ret = max(ret, func(x, y, 0) + score);
-    }
-    else {
-        if(x + 1 <= N - 1) {
-            ret = max(ret, func(x + 1, y, 0) + score);
-        }
-
-        if(y + 1 <= M - 1) {
-            ret = max(ret, func(x, y+1, 0) + score);
-        }
-    }
-
-    return ret;
-}
-
-void Print() {
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
+int dp[1002][1002];
+int graph[1002][1002];
 
 void solve() {
-    cout << func(N-1, 0, 1) << endl;
+    dp[N-1][0] = graph[N-1][0];
+
+    for(int i = N-1; i >= 0; i--) {
+        for(int j = 0; j < M; j++) {
+            if(j != 0) dp[i][j] = max(dp[i][j], dp[i][j-1] + graph[i][j]);      // 왼쪽에서 오는 경우
+            if(i != N-1) dp[i][j] = max(dp[i][j], dp[i+1][j] + graph[i][j]);    // 아래에서 오는 경우
+        }
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            dp[i][j] += graph[i][j];
+        }
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            if(j != 0) dp[i][j] = max(dp[i][j], dp[i][j-1] + graph[i][j]);
+            if(i != 0) dp[i][j] = max(dp[i][j], dp[i-1][j] + graph[i][j]);
+        }
+    }
+
+    cout << dp[N-1][M-1];
 }
 
 void input() {
     cin >> N >> M;
 
-    memset(dp, -1, sizeof(dp));
-
     for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
+        for(int j = 0; j < M; j++)  {
             cin >> graph[i][j];
+            dp[i][j] = -MAX;
         }
     }
 }
