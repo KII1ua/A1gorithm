8c0b42a91cf3bf6a61320e569d656937374edfc5
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-11-06 02:43:25 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+    int r;
+};
+
+struct halloween {
+    int cnt;
+    int score;
+};
+
+// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
+// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+// int dx[] = {0, 0, 1, -1};
+// int dy[] = {1, -1, 0, 0};
+int dx[] = {0, -1, 1, 0};
+int dy[] = {1, 0, 0, 1};
+
+void solve() {
+
+}
+
+void input() {
+    cout << "I'm Sexy";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
