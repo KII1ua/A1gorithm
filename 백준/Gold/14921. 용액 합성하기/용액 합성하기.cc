1949ff6f14290c07d5b4a2787590496f973a2655
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-11-13 10:24:12 +0900

    [Gold V] Title: 용액 합성하기, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+    int r;
+};
+
+struct halloween {
+    int cnt;
+    int score;
+};
+
+// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
+// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int N;
+vector<int> v;
+
+void solve() {
+    sort(v.begin(), v.end());
+
+    int answer = MAX;
+    int lo = 0;
+    int hi = v.size() -1;
+    int state = 0;
+
+    while(lo < hi) {
+        int stand = v[lo] + v[hi];
+
+        if(answer > abs(stand)) {
+            if(stand < 0) {
+                state = 1;
+            }
+            else state = 0;
+            answer = min(answer, abs(stand));
+        }
+
+        if(stand > 0) {
+            hi--;
+        }
+        else lo++;
+    }
+
+    cout << ((state == 0)? answer : answer * -1);
+}
+
+void input() {
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
