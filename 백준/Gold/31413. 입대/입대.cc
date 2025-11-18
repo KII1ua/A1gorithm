eff0e44a28503aee0d5257494e9fbc1f0740c99e
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-11-19 00:09:28 +0900

    [Gold III] Title: 입대, Time: 8 ms, Memory: 19256 KB -BaekjoonHub

---
+++ 백준/Gold/31413. 입대/입대.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+    int r;
+};
+
+struct halloween {
+    int cnt;
+    int score;
+};
+
+// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
+// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int N, M, A, D;
+int arr[2100];
+int dp[2100][2100];     // 헌혈을 i번 했을때 날짜가 j까지고 누적 가산점 value
+
+void Print() {
+    for(int i = 0; i <= N; i++) {
+        for(int j = 0; j <= N+D; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void solve() {
+    for(int j = 1; j < N+D; j++) {
+        dp[0][j] = dp[0][j-1] + arr[j];
+        
+        if(dp[0][j] >= M) {
+            cout << 0;
+            return;
+        }
+    }
+
+    for(int i = 1; i <= N; i++) {       // 헌혈 횟수
+        for(int j = 1; j < N+D; j++) {
+            dp[i][j] = max(dp[i][j], dp[i][j-1] + arr[j]);
+            if(j < D) continue;
+
+            dp[i][j] = max(dp[i][j], dp[i-1][j-D] + A);
+
+            if(dp[i][j] >= M) {
+                cout << i;
+                return;
+            }
+        }
+    }
+
+    cout << -1;
+}
+
+void input() {
+    cin >> N >> M;
+
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    cin >> A >> D;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
