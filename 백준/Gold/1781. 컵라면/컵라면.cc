e67753a7be4ee32caaa3ceb3983b53bef2317185
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-11-25 20:10:56 +0900

    [Gold II] Title: 컵라면, Time: 64 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -11,40 +11,34 @@ struct coordinate {
     int r;
 };
 
-struct horse {
-    int x;
-    int y;
-    int dir;
+struct halloween {
+    int cnt;
+    int score;
 };
 
 // int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
 // int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
-// int dx[] = {0, 0, 1, -1};
-// int dy[] = {1, -1, 0, 0};
-int dx[] = {1, 0};
-int dy[] = {0, 1};
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 int N;
 vector<pii> v;
-priority_queue<int, vector<int>, greater<int>> pq;
-
-void Print() {
-    for(auto it : v) {
-        cout << it.first << " " << it.second << endl;
-    }
-}
 
 void solve() {
     sort(v.begin(), v.end());
 
-    for(int i = 0; i < N; i++) {
-        int day = v[i].first;
-        int cost = v[i].second;
-        pq.push(cost);
+    priority_queue<int, vector<int>, greater<int>> pq;
 
-        if(day < pq.size()) pq.pop();
+    for(auto it : v) {
+        int day = it.first;
+        int cost = it.second;
+        pq.push(it.second);
+
+        if(day < pq.size()) {
+            pq.pop();
+        }
     }
 
-    int answer = 0;
+    ll answer = 0;
     while(!pq.empty()) {
         answer += pq.top();
         pq.pop();
