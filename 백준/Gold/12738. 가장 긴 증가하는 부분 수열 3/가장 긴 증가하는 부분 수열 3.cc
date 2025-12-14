66cd7d45f1d644614c661f80babf271d739f40a9
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-12-15 00:34:30 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 176 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+
+const int INF = 1e9;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int N;
+int arr[1000001];
+
+void solve() {
+    vector<int> v;
+
+    for(int i = 0; i < N; i++) {
+
+        if(v.empty()) {
+            v.push_back(arr[i]);
+        }
+        else {
+            if(v[v.size()-1] < arr[i]) {
+                v.push_back(arr[i]);
+            }
+            else {
+                int lo = -1;
+                int hi = v.size()-1;
+
+                while(lo + 1 < hi) {
+                    int mid = (lo + hi) / 2;
+
+                    if(arr[i] <= v[mid]) {
+                        hi = mid;
+                    }
+                    else lo = mid;
+                }
+
+                v[hi] = arr[i];
+            }
+        }
+    }
+
+    cout << v.size();
+}
+
+void input() {
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
