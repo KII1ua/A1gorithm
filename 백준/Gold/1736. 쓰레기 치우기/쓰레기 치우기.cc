6c2538e8946109cc54914644e9191f3423751192
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-12-22 23:17:32 +0900

    [Gold II] Title: 쓰레기 치우기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.cc
+++ 백준/Gold/1736. 쓰레기 치우기/쓰레기 치우기.cc
@@ -3,59 +3,36 @@ using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
 #define endl "\n"
-#define MAX 1e9
 
-struct coordinate {
-    int x;
-    int y;
-    int r;
-};
-
-struct halloween {
-    int cnt;
-    int score;
-};
-
-// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
-// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-int N, M;
+const int INF = 1e9;
+// int dx[] = {0, 0, 1, -1};
+// int dy[] = {1, -1, 0, 0};
+int dx[] = {1, 0};
+int dy[] = {0, 1};
+int N, M, trash, answer;
 int graph[101][101];
-int trash;
 
-void dfs(int x, int y) {
-    if(x == N-1 && y == M-1) {
-        if(graph[x][y] == 1) {
-            trash--;
-            graph[x][y] = 0;
-        }
-        return;
-    }
+void func(int x, int y) {
 
     for(int i = x; i < N; i++) {
         for(int j = y; j < M; j++) {
-            if(graph[i][j] == 1) {
-                graph[i][j] = 0;
+            if(graph[i][j]) {
                 trash--;
-                dfs(i, j);
+                graph[i][j] = 0;
+                func(i, j);
                 return;
             }
         }
     }
-
-    return;
 }
 
 void solve() {
-    int cnt = 0;
-
     while(trash > 0) {
-        dfs(0, 0);
-        cnt++;
+        func(0, 0);
+        answer++;
     }
 
-    cout << cnt;
+    cout << answer;
 }
 
 void input() {
@@ -65,7 +42,9 @@ void input() {
         for(int j = 0; j < M; j++) {
             cin >> graph[i][j];
 
-            if(graph[i][j] == 1) trash++;
+            if(graph[i][j] == 1) {
+                trash++;
+            }
         }
     }
 }
