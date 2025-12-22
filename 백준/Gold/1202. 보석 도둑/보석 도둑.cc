30c0cc085b4233a52fa37875cf0049e564f72e37
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-12-22 19:48:19 +0900

    [Gold II] Title: 보석 도둑, Time: 152 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -3,27 +3,14 @@ using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
 #define endl "\n"
-#define MAX 1e9
 
-struct coordinate {
-    int x;
-    int y;
-    int r;
-};
-
-struct halloween {
-    int cnt;
-    int score;
-};
-
-// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
-// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+const int INF = 1e9;
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 int N, K;
-vector<pii> jewel;
-int bag[300001];
+vector<pii> v;
 priority_queue<int> pq;
+int arr[300001];
 
 bool compare(pii a, pii b) {
     if(a.first == b.first) {
@@ -33,26 +20,35 @@ bool compare(pii a, pii b) {
     return a.first < b.first;
 }
 
-void solve() {
-    sort(jewel.begin(), jewel.end(), compare);
-    sort(bag, bag+K);
+void Print() {
+    for(auto it : v) {
+        cout << it.first << " " << it.second << endl;
+    }
 
-    ll answer = 0;
-    int idx = 0;
     for(int i = 0; i < K; i++) {
-        int bag_size = bag[i];
+        cout << arr[i] << " ";
+    }
+}
+
+void solve() {
+    sort(v.begin(), v.end(), compare);
+    sort(arr, arr+K);
+
+    int idx = 0;
+    ll answer = 0;
+
+    for(int i = 0; i < K; i++) {
+        int bagsize = arr[i];
 
         while(idx < N) {
-            int weight = jewel[idx].first;
-            int cost = jewel[idx].second;
+            int weight = v[idx].first;
+            int cost = v[idx].second;
 
-            if(weight <= bag_size) {
+            if(weight <= bagsize) {
                 pq.push(cost);
                 idx++;
             }
-            else {
-                break;
-            }
+            else break;
         }
 
         if(!pq.empty()) {
@@ -70,11 +66,11 @@ void input() {
     for(int i = 0; i < N; i++) {
         int a, b;
         cin >> a >> b;
-        jewel.push_back({a, b});
+        v.push_back({a, b});
     }
 
     for(int i = 0; i < K; i++) {
-        cin >> bag[i];
+        cin >> arr[i];
     }
 }
 
