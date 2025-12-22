c2230e03d83bbc93a3043c8518e8b0f0ae8fda6c
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-12-23 00:40:50 +0900

    [Gold II] Title: 대표 선수, Time: 208 ms, Memory: 6080 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.cc
+++ 백준/Gold/2461. 대표 선수/대표 선수.cc
@@ -3,41 +3,19 @@ using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
 #define endl "\n"
-#define MAX 1e9
 
-struct coordinate {
-    int x;
-    int y;
-    int r;
-};
-
-struct halloween {
-    int cnt;
-    int score;
-};
-
-// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
-// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
-// int dx[] = {0, 0, 1, -1};
-// int dy[] = {1, -1, 0, 0};
-int dx[] = {0, -1};
-int dy[] = {-1, 0};
+const int INF = 1e9;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 int N, M;
-vector<vector<int>> v(1001);
+int graph[1001][1001];
 int arr[1001];
-priority_queue<pii, vector<pii>> max_pq;
-priority_queue<pii, vector<pii>, greater<pii>> min_pq;
-set<int> st;
-int answer;
-
-bool compare(int a, int b) {
-    return a > b;
-}
+priority_queue<pii, vector<pii>, greater<pii>> pq;
 
 void Print() {
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
-            cout << v[i][j] << " ";
+            cout << graph[i][j] << " ";
         }
         cout << endl;
     }
@@ -45,29 +23,28 @@ void Print() {
 
 void solve() {
     for(int i = 0; i < N; i++) {
-        sort(v[i].begin(), v[i].end());
+        sort(graph[i], graph[i]+M);
     }
 
     int max_val = 0;
-    int answer = MAX;
-
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
 
     for(int i = 0; i < N; i++) {
-        pq.push({v[i][0], i});
-        max_val = max(v[i][0], max_val);
+        pq.push({graph[i][0], i});
+        max_val = max(max_val, graph[i][0]);
     }
 
+    int answer = max_val - pq.top().first;
+
     while(true) {
-        int min_val = pq.top().first;
+        int val = pq.top().first;
         int idx = pq.top().second;
         pq.pop();
 
-        answer = min(answer, max_val - min_val);
+        answer = min(answer, max_val - val);
 
         if(++arr[idx] < M) {
-            pq.push({v[idx][arr[idx]], idx});
-            max_val = max(max_val, v[idx][arr[idx]]);
+            pq.push({graph[idx][arr[idx]], idx});
+            max_val = max(max_val, graph[idx][arr[idx]]);
         }
         else {
             break;
@@ -82,9 +59,7 @@ void input() {
 
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
-            int a;
-            cin >> a;
-            v[i].push_back(a);
+            cin >> graph[i][j];
         }
     }
 }
