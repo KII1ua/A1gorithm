0a6b8ad9f0ff5213a7036ea2e82603ae9444b73d
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-12-25 18:53:00 +0900

    [Gold III] Title: 두 배, Time: 20 ms, Memory: 2996 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+
+struct Tree {
+    int Node, left, right;
+};
+
+const int INF = 1e9;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int N, cnt;
+int arr[250001];
+
+void solve() {
+    for(int i = 1; i < N; i++) {
+        while(arr[i-1] > arr[i]) {
+            arr[i] *= 2;
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+}
+
+void input() {
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
