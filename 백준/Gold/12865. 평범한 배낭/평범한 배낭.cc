836d808a006e6622905c73b0537f18ea49bc2a9d
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-12-28 23:54:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,36 +1,48 @@
-// baekjoon 17835
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> pii;
 typedef long long ll;
 #define endl "\n"
-#define INF int(1e9)
 
-int dx[8] = {-1, 1, 0, 0, -1, -1, 1, 1};
-int dy[8] = {0, 0, -1, 1, -1, 1, -1, 1};
+struct Tree {
+    int Node, left, right;
+};
+
+const int INF = 1e9;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 int N, K;
-int dp[101][100001];
-int W[101];
-int V[101];
+int dp[100001];
+vector<pii> v;
+
+void solve() {
+    for(int i = 0; i < N; i++) {
+        int weight = v[i].first;
+        int cost = v[i].second;
+
+        for(int j = K; j >= 0; j--) {
+            if(j - weight < 0) continue;
+            dp[j] = max(dp[j], dp[j - weight] + cost);
+        }
+    }
+
+    cout << dp[K];
+}
+
+void input() {
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
-    cin >> N >> K;
-
-    for(int i = 1; i <= N; i++) {
-        cin >> W[i] >> V[i];
-    }
-
-    for(int i = 1; i <= N; i++) {
-        for(int j = 1; j <= K; j++) {
-            if(j - W[i] >= 0)
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
-            else
-                dp[i][j] = dp[i-1][j];
-        }
-    }
-
-    cout << dp[N][K];
+    input();
+    solve();
 }
