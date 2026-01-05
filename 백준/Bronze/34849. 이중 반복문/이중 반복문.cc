a889224e4e773cc2f011c78e765de8ae51345586
황성윤 <86961575+KII1ua@users.noreply.github.com>
2026-01-06 03:47:45 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+
+struct Tree {
+    int Node, left, right;
+};
+
+const int INF = 1e9;
+const int MAX = 123457;
+const int MOD = 1e9 + 7;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+ll N;
+
+void solve() {
+    if(N * N > 1e8) {
+        cout << "Time limit exceeded";
+    }
+    else cout << "Accepted";
+}
+
+void input() {
+    cin >> N;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
