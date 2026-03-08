e87dc2313c838fb237bf75aa4ec228413781a797
황성윤 <86961575+KII1ua@users.noreply.github.com>
2026-03-08 17:42:34 +0900

    [Gold V] Title: 퇴사 2, Time: 204 ms, Memory: 25460 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -16,23 +16,6 @@ int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 int N;
 
-ll func(int idx, vector<ll> &dp, vector<pii> &v) {
-    if(idx > N) return -INF;
-    ll &ret = dp[idx];
-
-    if(ret != -1) return ret;
-
-    ret = 0;
-
-    ret = max(ret, func(idx+1, dp, v));        // 선택하지 않은 경우
-
-    if(v[idx].first + idx <= N) {
-        ret = max(ret, func(idx + v[idx].first, dp, v) + v[idx].second);
-    }
-
-    return ret;
-}
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
@@ -40,11 +23,25 @@ int main() {
     cin >> N;
 
     vector<pii> v(N, {0, 0});
-    vector<ll> dp(N+1, -1);
+    vector<ll> dp(N+1, 0);
 
     for(auto &iter : v) {
         cin >> iter.first >> iter.second;
     }
 
-    cout << func(0, dp, v);
+    if(v[0].first <= N)  {
+        dp[v[0].first] = max(dp[0] + v[0].second, dp[v[0].first]);
+    }
+
+    for(int i = 1; i < N; i++) {
+        // 안하는 경우
+        if(i+1 <= N) dp[i+1] = max(dp[i], dp[i+1]);
+
+        // 하는경우
+        if(i + v[i].first <= N) {
+            dp[i+v[i].first] = max(dp[i] + v[i].second, dp[i+v[i].first]);
+        }
+    }
+
+    cout << dp[N];
 }
