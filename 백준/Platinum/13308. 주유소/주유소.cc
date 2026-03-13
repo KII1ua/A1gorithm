43b1e22b2fe3717b61a8cc7d0e126b9569dfb3e6
황성윤 <86961575+KII1ua@users.noreply.github.com>
2026-03-13 19:45:03 +0900

    [Platinum V] Title: 주유소, Time: 20 ms, Memory: 26904 KB, Score: 34 point -BaekjoonHub

---
+++ 백준/Platinum/13308. 주유소/주유소.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+
+struct Tree {
+    int Node, left, right;
+};
+
+const int INF = 1e9;
+const int MAX = 2501;
+const int D = 2000000;
+const int MOD = 10007;
+// int dx[] = {0, 0, 1, -1};
+// int dy[] = {1, -1, 0, 0};
+int dx[] = {1, 0, 0};
+int dy[] = {0, -1, 1};
+int N, M;
+vector<vector<pii>> v(MAX);
+int price[MAX];
+int dp[MAX][MAX];       // i까지 왔을때 지나쳐온 주유소중 j가격의 최소 비용
+
+void dijkstra() {
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+    pq.push({price[1], 1, 0});     // 비용, 노드, 가격
+    dp[1][price[1]] = 0;
+
+    while(!pq.empty()) {
+        int mincost = get<0>(pq.top());
+        int node = get<1>(pq.top());
+        int cost = get<2>(pq.top());
+        pq.pop();
+
+        if(dp[node][mincost] < cost) continue;
+
+        for(auto &iter : v[node]) {
+            int nextnode = iter.first;
+            int nextmincost = min(mincost, price[nextnode]);
+            int nextcost = cost + iter.second * mincost;
+
+            if(dp[nextnode][nextmincost] > nextcost) {
+                pq.push({nextmincost, nextnode, nextcost});
+                dp[nextnode][nextmincost] = nextcost;
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    cin >> N >> M;
+
+    for(int i = 1; i <= N; i++) {
+        cin >> price[i];
+    }
+
+    for(int i = 0; i < M; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v[a].push_back({b, c});
+        v[b].push_back({a, c});
+    }
+
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= MAX; j++) {
+            dp[i][j] = INF;
+        }
+    }
+
+    dijkstra();
+
+    int answer = INF;
+
+    for(int i = 0; i <= MAX; i++) {
+        answer = min(answer, dp[N][i]);
+    }
+
+    cout << answer;
+}
