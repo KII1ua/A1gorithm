be57b64c36c79efdde698649d957468a01b72a0e
황성윤 <86961575+KII1ua@users.noreply.github.com>
2026-03-22 17:03:35 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,10 +1,7 @@
 -- 코드를 입력하세요
-
--- 1. 서울에 위치한 식당들의 식당 ID, 식당 이름, 음식 종류, 즐겨찾기 수, 주소, 리뷰 평균 점수를 조회
--- 2. 리뷰 평균점수는 소수점 세번째 자리에서 반올림 후 결과는 평균 점수를 기준으로 내림차순 정렬
--- 3. 만약 평균점수가 같다면 즐겨찾기 수를 기준으로 내림차순 정렬
-
-
--- 1. SELECT a.REST_ID, b.REST_NAME, b.FOOD_TYPE, b.FAVORITES, b.ADDRESS from REST_REVIEW a join REST_INFO b on a.REST_ID = b.REST_ID;
-
-SELECT a.REST_ID, b.REST_NAME, b.FOOD_TYPE, b.FAVORITES, b.ADDRESS, ROUND(AVG(a.REVIEW_SCORE), 2) as SCORE from REST_REVIEW a join REST_INFO b on a.REST_ID = b.REST_ID group by a.REST_ID HAVING b.ADDRESS LIKE "서울%" ORDER BY SCORE DESC, B.FAVORITES DESC;
+select l.rest_id, l.rest_name, l.food_type, l.favorites, l.address, rtb.score from rest_info l
+	join (SELECT rest_id, ROUND(avg(review_score), 2) as score from rest_review
+	group by rest_id) as rtb
+    on l.rest_id = rtb.rest_id
+    where l.address like '서울%'
+    order by rtb.score desc, l.favorites desc;
